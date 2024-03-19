644bcfcd8e56b7c41a866912a13e8bc9c863b3a0
An Luu <atluutran@gmail.com>
2024-03-18 20:59:38 -0400

    Test

---
+++ test-1/test_1App.swift
@@ -0,0 +1,17 @@
+//
+//  test_1App.swift
+//  test-1
+//
+//  Created by An Luu on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct test_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
